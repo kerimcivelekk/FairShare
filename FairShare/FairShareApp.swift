5dfbadcb03ebf9a799fcafcd1907673fdee65225
Kerim <targetbad@hotmail.com>
2024-09-27 00:57:51 +0300

    Initial Commit

---
+++ FairShare/FairShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  FairShareApp.swift
+//  FairShare
+//
+//  Created by Kerim Civelek on 27.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct FairShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FairShareTests/FairShareTests.swift
@@ -0,0 +1,17 @@
+//
+//  FairShareTests.swift
+//  FairShareTests
+//
+//  Created by Kerim Civelek on 27.09.2024.
+//
+
+import Testing
+@testable import FairShare
+
+struct FairShareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
